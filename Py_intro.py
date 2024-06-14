e5886ed46ecd130649e66a3b7032ecb6b77f3d3b
MD Subhanoddin <141422249+monkeyDsubhan@users.noreply.github.com>
2024-06-14 10:49:20 +0530

    Add files via upload
    The First day introduction to python

---
+++ Py_intro.py
@@ -0,0 +1,40 @@
+#====BASICS OF PYTHON==========
+#print("hello world")
+#variable name starts with lowrcase or uppercase or underscore  only 
+# a=0000
+# print(d)
+#============COUNT VOWELS=========
+# Program to count the number of each vowels
+
+# string of vowels
+# vowels = 'aeiou'
+
+# ip_str = 'Hello, have you tried our tutorial section yet?'
+# d={j:0 for j in vowels}
+# for i in ip_str:
+#     if i in vowels:
+#         cunt+=1
+# print(d)
+
+#swap 2 numbers using XOR operator
+# a=10
+# b=20
+# a=a^b
+# b=b^a
+# a=a^b
+# print(a)
+# print(b)
+#==============LEAP YEAR========
+# year=int(input())
+# if year%4==0:
+#     if year%100==0:
+#         if year%400==0:
+#             print(year,"is a leap year")
+#         else:
+#             print("not leap year")
+#     else:
+#         print(year,"leap year")
+# else:
+#     print("not a leap year")
+
+
